29a8621c539e518fbdedd44a43c5f17585bef1c4
Michał Siatkowski <atais.jr@gmail.com>
2016-12-20 10:05:18 +0100

    Update delete-old-snapshots.sh

--- delete-old-snapshots.sh
+++ delete-old-snapshots.sh
@@ -20,7 +20,7 @@ IFS="${NIFS}"
 for vm in ${VM_IDS}
 do
 	echo "Deleting all but latest $1 snapshots from $vm"
-	VBoxManage snapshot "$vm" list | sed -e :a -e "$d;N;2,$1ba" -e 'P;D'  | awk -F "UUID: " '{print $2}' | awk -F ")" '{print $1}' | xargs -L1 VBoxManage snapshot "$vm" delete
+	VBoxManage snapshot "$vm" list | head -n -"$1" | awk -F "UUID: " '{print $2}' | awk -F ")" '{print $1}' | xargs -L1 VBoxManage snapshot "$vm" delete
 
 	logger "Finished snapshot deletion of $vm"
 	
